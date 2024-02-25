2a807d2cbcf3533e4042d54ec2b64d6dfbb6d020
dn00lives <160515011+dn00lives@users.noreply.github.com>
2024-02-25 11:16:33 +0800

    Add files via upload

---
+++ python/if elif else.py
@@ -0,0 +1,40 @@
+
+#
+#################################################################################
+
+#name = input ("What is you name?")
+
+#if name == "Josh":
+#    print ("Get out!\n")
+#else:
+#    print ("Hello " + name + ". You are welcome here.\n")
+
+#
+#################################################################################
+
+#age = int (input ("What is you age? "))
+
+#if age >= 18:
+#    print ("legal\n")
+#else:
+#    print ("minor\n")
+
+
+#################################################################################
+
+value = float (input ("Positive or Negative? "))
+
+if value >= 0.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001:
+    print ("Positive\n")
+
+elif value == 0:
+    print ("Zero\n")
+
+else:
+    print ("Negative\n")
+
+
+
+
+
+
