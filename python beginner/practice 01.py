9bc86f3538a5457a4831e8cda0d74799bc9b19bf
dn00lives <160515011+dn00lives@users.noreply.github.com>
2024-02-25 09:35:20 +0800

    Add files via upload

---
+++ python beginner/practice 01.py
@@ -0,0 +1,31 @@
+
+
+
+################################################
+
+space = " "
+
+intro = "My name is "
+
+first_name  = "Joshua"
+last_name   = "Rivera"
+full_name   = first_name + space + last_name
+
+period      = "."
+
+
+
+
+################################################
+
+print (intro + full_name + period)
+
+
+
+
+
+
+
+
+
+
