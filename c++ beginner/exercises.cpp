49e0a63a41ebf2328d32db162069d82a927e908b
dn00lives <160515011+dn00lives@users.noreply.github.com>
2024-02-25 09:38:07 +0800

    Add files via upload

---
+++ c++ beginner/exercises.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+
+
+
+int main(){
+
+    int number = -1;
+    
+    if (number >=1) {
+        std::cout <<"zero" << '\n';
+    } 
+    else {
+        std::cout <<"negative" << '\n';
+    }
+   
+
+    
+    
+
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
