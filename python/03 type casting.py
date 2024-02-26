cced974a5c803d51de591b825156b5f5c53f4e7b
dn00lives <160515011+dn00lives@users.noreply.github.com>
2024-02-26 13:55:16 +0800

    Add files via upload

---
+++ python/03 type casting.py
@@ -0,0 +1,23 @@
+
+# type casting - convert the data type of a value to another data type.
+
+
+x = 1           #int - whole number
+y = 2.1         #float - number with decimal
+z = "3"         #str - statements
+
+x = str(x)      # convert int to str
+y = int(y)      # convert float to int
+z = float(z)    # convert str to float
+
+print(x*6)
+print(y*2)
+print(z*3.1)
+
+
+
+
+
+
+
+
