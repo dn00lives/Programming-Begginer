127b36becc80d6de7aa572c84db187d5a81d411f
dn00lives <160515011+dn00lives@users.noreply.github.com>
2024-02-26 11:27:54 +0800

    Add files via upload

---
+++ python/01 variables.py
@@ -0,0 +1,80 @@
+
+
+# A VARIABLE is a container for value. It behaves as the value that it contains.
+
+
+######################################################
+space = " "
+print (space)
+########## strings / str / "" ########################
+
+space = " "
+period = "."
+
+intro = "My name is "
+
+first_name = "Joshua"
+last_name = "Rivera"
+full_name = first_name + space + last_name
+
+print (intro + full_name + period)
+print (type(intro))
+print ("(The 'str' is Joshua)")
+
+
+######################################################
+space = " "
+print (space)
+########## interger / int ############################
+
+intro = "I am "
+
+age = 33
+age += 1
+
+print (intro + str(age) + " yrs old.")
+print (type(age))
+print ("(The 'int' is 34)")
+
+
+######################################################
+space = " "
+print (space)
+########## float / float #############################
+
+intro = "My height is "
+
+height = 60.03
+
+print (intro + str(height) + "cm.")
+print (type(height))
+print ("(The 'float' is 60.03)")
+
+
+######################################################
+space = " "
+print (space)
+########## boolean / bool / True False ###############
+
+intro = "Am I a human?"
+
+human = True
+
+print (intro)
+print ("Answer: " + str(human))
+print (type(human))
+print ("(The 'bool' is True)")
+
+
+######################################################
+
+
+
+
+
+
+
+
+
+
+
